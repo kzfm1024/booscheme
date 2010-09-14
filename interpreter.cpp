12216f61d38e1a59eca0f82f24aa11d38d1f22a0
kzfm1024 <kzfm1024@gmail.com>
2010-09-15 00:45:27 +0900

    eval() implementation (TENTATIVE)

--- interpreter.cpp
+++ interpreter.cpp
@@ -15,7 +15,7 @@ void interpreter::repl()
 {
     boost::any x;
 
-    while (1)
+    while (true)
     {
         try
         {
@@ -33,10 +33,87 @@ void interpreter::repl()
 
 boost::any interpreter::eval(boost::any x, Environment env)
 {
-    return x; // NOT YET
+    //
+    // The purpose of the while loop is to allow tail recursion.
+    // The idea is that in a tail recursive position, we do "x = ..."
+    // and loop, rather than doing "return eval(...)".
+    //
+    while (true)
+    {
+        if (isSymbol(x))
+        {
+            return env->lookup(sym(x));
+        }
+        else if (!isPair(x))
+        {
+            return x;
+        }
+        else
+        {
+            boost::any fn = first(x);
+            boost::any args = rest(x);
+            if (isSymbol(fn, "quote")) // QUOTE
+            {
+                return first(args);
+            }
+            else if (isSymbol(fn, "begin")) // BEGIN
+            {
+                for (; !isEmpty(rest(args)); args = rest(args))
+                {
+                    eval(first(args), env);
+                }
+                x = first(args);
+            }
+            else if (isSymbol(fn, "define")) // DEFINE
+            {
+                // NOT YET
+            }
+            else if (isSymbol(fn, "set!")) // SET!
+            {
+                // NOT YET
+            }
+            else if (isSymbol(fn, "if")) // IF
+            {
+
+            }
+            else if (isSymbol(fn, "cond")) // COND
+            {
+
+            }
+            else if (isSymbol(fn, "lambda")) // LAMBDA
+            {
+
+            }
+            else if (isSymbol(fn, "macro"))
+            {
+
+            }
+            else
+            {
+                fn = eval(fn, env);
+                if (isMacro(fn))
+                {
+
+                }
+                else if (isClosure(fn))
+                {
+
+                }
+                else
+                {
+                    return x; // NOT YET
+                }
+            }
+        }
+    }
 }
 
 boost::any interpreter::eval(boost::any x)
 {
     return eval(x, globalEnvironment);
 }
+
+//
+// evalList()
+// reduceCond()
+//
