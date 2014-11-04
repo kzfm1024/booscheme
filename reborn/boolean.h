f68ed381dee3123fe0b71ac1bacf428081418ff3
Kazufumi Cho <kzfm1024@gmail.com>
2014-11-05 02:09:54 +0900

    added boolean.h

---
+++ reborn/boolean.h
@@ -0,0 +1,29 @@
+//
+// boolean.h
+//
+
+#pragma once
+#ifndef _BOOLEAN_H
+#define _BOOLEAN_H
+
+#include <string>
+#include <map>
+#include "object.h"
+
+namespace booscheme
+{
+	class boolean : public object
+	{
+	public:
+		boolean(bool val) : m_val(val) {}
+		virtual ~boolean() {} 
+
+		virtual std::string to_s() { return m_val ? std::string("#t") : std::string("#f"); }
+		virtual std::string inspect() { return this->to_s(); }
+		
+	private:
+		bool m_val;
+	};
+}
+
+#endif // _BOOLEAN_H
