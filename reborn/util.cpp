b1082c19c1f839e2d9ff778856c518b3a6e3139c
Kazufumi Cho <kzfm1024@gmail.com>
2014-11-06 01:08:44 +0900

    added cons(), car(), ...

--- reborn/util.cpp
+++ reborn/util.cpp
@@ -2,7 +2,9 @@
 // util.cpp
 //
 
+#include <assert.h>
 #include "util.h"
+#include "pair.h"
 
 namespace booscheme
 {
@@ -23,5 +25,38 @@ namespace booscheme
 	boolean* BOOLEAN(bool x)
 	{
 		return x ? TRUE() : FALSE();
+	}
+
+	object* cons(object* a, object* b)
+	{
+		return new pair(a, b);
+	}
+
+	object* car(object* x)
+	{
+		pair* p = dynamic_cast<pair*>(x);
+		assert(p);
+		return p->car();
+	}
+
+	object* cdr(object* x)
+	{
+		pair* p = dynamic_cast<pair*>(x);
+		assert(p);
+		return p->cdr();
+	}
+
+	object* setcar(object* x, object* a)
+	{
+		pair* p = dynamic_cast<pair*>(x);
+		assert(p);
+		return p->setcar(a);
+	}
+
+	object* setcdr(object* x, object* b)
+	{
+		pair* p = dynamic_cast<pair*>(x);
+		assert(p);
+		return p->setcdr(b);
 	}
 }
--- reborn/util.h
+++ reborn/util.h
@@ -13,6 +13,12 @@ namespace booscheme
 	boolean* TRUE();
 	boolean* FALSE();
 	boolean* BOOLEAN(bool x);
+
+	object* cons(object* a, object* b);
+	object* car(object* x);
+	object* cdr(object* x);
+	object* setcar(object* x, object* a);
+	object* setcdr(object* x, object* b);
 }
 
 #endif // _UTIL_H
