1767c5286cf66b8173dc100278aa995e5258268a
Kazufumi Cho <kzfm1024@gmail.com>
2014-11-11 00:05:02 +0900

    updated boo.h and boo.cpp

--- reborn/boo.cpp
+++ reborn/boo.cpp
@@ -2,9 +2,21 @@
 // boo.cpp
 //
 
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
 #include <assert.h>
 #include "boo.h"
 
+#include "boolean.h"
+#include "null.h"
+#include "pair.h"
+#include "number.h"
+#include "character.h"
+#include "symbol.h"
+#include "misc.h"
+#include "environment.h"
+#include "procedure.h"
 
 namespace boo
 {
@@ -34,7 +46,26 @@ namespace boo
 		return nil;
 	}
 
-	object* cons(object* a, object* b)
+	object* UNDEF()
+	{
+		static misc* undef;
+		if (!undef) undef = new misc("#<undef>");
+		return dynamic_cast<object*>(undef);
+	}
+
+	misc* error(const std::string& message)
+	{
+		throw std::runtime_error("*** ERROR: " + message);
+		return new misc("#<error>");
+	}
+
+	misc* warn(const std::string& message)
+	{
+		std::cerr << "*** WARNING: " << message << std::endl;
+		return new misc("#<warn>");
+	}
+
+	pair* cons(object* a, object* b)
 	{
 		return new pair(a, b);
 	}
@@ -77,5 +108,116 @@ namespace boo
 	{
 		pair* p = dynamic_cast<pair*>(x);
 		return p ? true : false;
+	}
+
+	bool is_number(object* x)
+	{
+		number* num = dynamic_cast<number*>(x);
+		return num ? true : false;
+	}
+
+	bool is_character(object* x)
+	{
+		character* ch = dynamic_cast<character*>(x);
+		return ch ? true : false;
+	}
+
+	bool is_symbol(object* x)
+	{
+		symbol* sym = dynamic_cast<symbol*>(x);
+		return sym ? true : false;
+	}
+
+	bool is_symbol(object* x, const std::string& s)
+	{
+		symbol* sym = dynamic_cast<symbol*>(x);
+		return (sym && sym->to_s() == s) ? true : false;
+	}
+
+	bool is_procedure(object* x, const std::string& s)
+	{
+		procedure* proc = dynamic_cast<procedure*>(x);
+		return (proc && proc->to_s() == s) ? true : false;
+	}
+
+	static void stringify_pair(pair* p, bool quoted, std::ostringstream& buf);
+
+	static void stringify(object* x, bool quoted, std::ostringstream& buf)
+	{
+		if (is_pair(x))
+		{
+			stringify_pair(dynamic_cast<pair*>(x), quoted, buf);
+		}
+		else if (is_character(x))
+		{
+			if (quoted) buf << "#\\"; // buf.append("#\\");
+			buf << x->to_s();
+		}
+		else
+		{
+			buf << x->to_s();
+		}
+	}
+
+	static void stringify_pair(pair* p, bool quoted, std::ostringstream& buf)
+	{
+		std::string special;
+
+		if (is_pair(p->cdr()) && is_null(cdr(p->cdr())))
+		{
+			if      (is_symbol(p->car(), "quote"))            { special = "'";  }
+			else if (is_symbol(p->car(), "quasiquote"))       { special = "`";  }
+			else if (is_symbol(p->car(), "unquote"))          { special = ",";  }
+			else if (is_symbol(p->car(), "unquote-splicing")) { special = ",@"; }
+		}
+
+		if (special.size())
+		{
+			buf << special;
+			stringify(car(p->cdr()), quoted, buf);
+		}
+		else
+		{
+			buf << "(";
+			stringify(p->car(), quoted, buf);
+
+			object* tail = p->cdr();
+			while (is_pair(tail))
+			{
+				buf << " ";
+				stringify(car(tail), quoted, buf);
+				tail = cdr(tail);
+			}
+
+			if (!is_null(tail))
+			{
+				buf << " . ";
+				stringify(tail, quoted, buf);
+			}
+
+			buf << ")";
+		}
+	}
+
+	std::string stringify(object* x, bool quoted)
+	{
+		std::ostringstream buf;
+		stringify(x, quoted, buf);
+		return buf.str();
+	}
+
+	std::string stringify(object* x)
+	{
+		return stringify(x, true);
+	}
+
+	void print(object* x)
+	{
+		std::cout << stringify(x) << std::endl << std::flush;
+	}
+
+	void print(const std::string& msg, object* x)
+	{
+		std::cout << msg << ": " << stringify(x) << std::endl << std::flush;
 	}
 }
--- reborn/boo.h
+++ reborn/boo.h
@@ -6,19 +6,25 @@
 #ifndef _BOO_H
 #define _BOO_H
 
-#include "boolean.h"
-#include "null.h"
-#include "pair.h"
-#include "symbol.h"
+#include <string>
 
 namespace boo
 {
+	class object;
+	class boolean;
+	class null;
+	class misc;
+	class pair;
+
     boolean* TRUE();
     boolean* FALSE();
     boolean* BOOLEAN(bool x);
     null* NIL();
+	object* UNDEF();
+	misc* error(const std::string& message);
+	misc* warn(const std::string& message);
 
-	object* cons(object* a, object* b);
+	pair* cons(object* a, object* b);
     object* car(object* x);
 	object* cdr(object* x);
 	object* setcar(object* x, object* a);
@@ -26,6 +32,18 @@ namespace boo
 
     bool is_null(object* x);
     bool is_pair(object* x);
+    bool is_number(object* x);
+    bool is_character(object* x);
+    bool is_number(object* x);
+	bool is_symbol(object* x);
+	bool is_symbol(object* x, const std::string& s);
+	bool is_procedure(object* x, const std::string& s);
+
+	std::string stringify(object* x, bool quoted);
+	std::string stringify(object* x);
+
+	void print(object* x);
+	void print(const std::string& msg, object* x);
 }
 
 #endif // _BOO_H
