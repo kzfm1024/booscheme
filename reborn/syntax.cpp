bdb30454889708702db7273e3ffce3c6f14d6ab2
Kazufumi Cho <kzfm1024@gmail.com>
2014-11-25 13:31:37 +0900

    added syntax.{h,cpp}

---
+++ reborn/syntax.cpp
@@ -0,0 +1,29 @@
+//
+// syntax.cpp
+//
+
+#include <sstream>
+#include "boo.h"
+#include "boo_types.h"
+#include "syntax.h"
+
+namespace boo
+{
+	syntax::syntax(const std::string& name, object* syntax_rules) :
+		m_name(name),
+		m_syntax_rules(syntax_rules)
+	{
+	}
+
+	std::string syntax::to_s()
+	{
+		std::ostringstream stream;
+		stream << "#<syntax " << m_name << ">";
+		return stream.str();
+	}
+
+	object* syntax::expand(object* x)
+	{
+		return x; // NOT YET
+	}
+}
---
+++ reborn/syntax.h
@@ -0,0 +1,30 @@
+//
+// syntax.h
+//
+
+#pragma once
+#ifndef _SYNTAX_H
+#define _SYNTAX_H
+
+#include "object.h"
+#include "boo.h"
+
+namespace boo
+{
+    class syntax : public object
+    {
+	public:
+		syntax(const std::string& name, object* syntax_rules);
+        virtual ~syntax() {} 
+        
+        virtual std::string to_s();
+        std::string name() { return m_name; }
+		object* expand(object* x);
+
+	private:
+        std::string m_name;
+		object* m_syntax_rules;
+    };
+}
+
+#endif // _SYNTAX_H
